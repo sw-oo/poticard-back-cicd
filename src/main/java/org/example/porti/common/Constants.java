4231df209803069d2669591959f926c037f94f1a
Tahcy-99 <sjae999@gmail.com>
2026-03-16 12:41:35 +0900

    [Feat] 카카오톡 소셜로그인 구현

---
+++ src/main/java/org/example/porti/common/Constants.java
@@ -0,0 +1,6 @@
+package org.example.porti.common;
+
+public class Constants {
+    public static final String DEFAULT_USER_ROLE = "ROLE_USER";
+    public static final String DEFAULT_COMPANY_ROLE = "ROLE_COMPANY";
+}
